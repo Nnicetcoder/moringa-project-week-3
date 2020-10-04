5f5435b9be1f76a4d08a95a27077cb9446fd9545
Your Name <nntwari75@gmail.com>
2020-10-04 14:13:24 -0700

    seventeenth commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("img src="design_icon.png"").click(function(){
+    $("#div1").fadeToggle();});
+
+  $("img src="dev_icon.png"" ).click(function(){
+    $("#div2").fadeToggle();});
+
+  $("img src="product_icon.png"").click(function(){
+    $("#div3").fadeToggle();});
+  
+});
+
+
+
+
